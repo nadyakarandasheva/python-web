36d9c40dd612d28fbd50c4f4168c8d81554ffe20
Nadya-Karandasheva <nadya.karandasheva.2000@mail.ru>
2024-11-10 14:16:47 +0300

    fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,8 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
-
+import html from "eslint-plugin-html"
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
-];
+  {
+    files: ["**/*.html"],
+    plugins: { html },
+  },
+]
